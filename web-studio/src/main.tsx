08f7c32be078482cf0887c87996c863f60a74430
Ferry <biyanci1@outlook.com>
2026-04-05 17:24:06 +0800

    feat: 导入基础的shadcn组件

--- web-studio/src/main.tsx
+++ web-studio/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { TooltipProvider } from './components/ui/tooltip'
 
 const router = createRouter({
   routeTree,
@@ -18,5 +19,5 @@ const rootElement = document.getElementById('app')!
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<RouterProvider router={router} />)
+  root.render(<TooltipProvider><RouterProvider router={router} /></TooltipProvider>)
 }
